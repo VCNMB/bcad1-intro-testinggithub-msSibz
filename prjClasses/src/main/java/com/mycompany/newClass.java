459fc1c32f93451a0a8f04303666ddca0804823d
msSibz <sibaganga@icloud.com>
2025-03-17 08:30:02 +0200

    Courtneys work
    courts homework

---
+++ prjClasses/src/main/java/com/mycompany/newClass.java
@@ -0,0 +1,21 @@
+package com.mycompany;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class newClass {
+    
+    public void helloClass {
+    
+}  system.out.println ("Hello from the 2nd class")
+
+
+            
+    
+}
---
+++ prjClasses/src/main/java/prjClasses1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class students (
+
+        private string strStudentName, strStudentCourse;
+
+private double dblStudentAverage;
+{
+public void setName (String strName)
+}
+
+strStudentName = strName;
+
+{
+public StringgetName () 
+}
+
+{
+return strStudentName
+}
+
+
+public void setAverage(double dblAverage)
+
+dblStudentAverage = dblAverage;
+
+public double getAverage()
+{
+return (dblStudentAverage + (dblStudentAverage *0.10));
+}
